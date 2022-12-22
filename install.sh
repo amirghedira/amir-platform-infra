29e1b66f3a52ea81b3c6fb30e66d171ae133913e
amirghedira <amirghedira06@gmail.com>
2022-12-22 13:03:27 +0100

    add environment variables

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ environment_set(){
     env="${1}"
     export PLAYBOOK="playbook-${env}.yml"
     export VAROPTS="@env/${env}.yml"
+    export SLACK_TOKEN=${CI_SLACK_TOKEN}
+    export MONGO_URI=${CI_MONGO_URI}
 }
 while getopts ":e:" option; do 
   case "$option" in 
